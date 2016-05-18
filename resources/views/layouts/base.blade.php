7e743a891619788420be48b603bc684ef6a9cf17
Cade Gillem <cpgillem@gmail.com>
2016-05-17 22:13:02 -0400

    Use proper resource loading in templates.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- BOOTSTRAP REQUIREMENTS -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- BOOTSTRAP REQUIREMENTS ^ -->
 
     <title>@yield('title') - Academic Computing Society</title>
 
     <!-- CSS STYLING -->
-    <link rel="stylesheet" src="bower_components/bootstrap/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" src="{{ asset('css/bootstrap.min.css') }}"/>
     <link rel="stylesheet" src="{{ asset('css/style.css') }}"/>
   </head>
   <body>
@@ -25,7 +25,7 @@
     </div>
 
     <!-- BOOTSTRAP JS -->
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   </body>
 </html>
