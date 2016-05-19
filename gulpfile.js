b90c90e192dcb2fe19e1b2a94511db9857788bf4
Cade Gillem <cpgillem@gmail.com>
2016-05-18 21:29:45 -0400

    Change paths in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,11 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
 	/* Stylesheets */
 	mix.sass('app.scss');
-	mix.copy('bower_components/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+	mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
 	mix.stylesIn('resources/assets/css/', 'public/css/style.css');
 
 	/* Scripts */
-	mix.copy('bower_components/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
-	mix.copy('bower_components/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+	mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
+	mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
 	mix.scripts('resources/assets/js/', 'public/js/scripts.js');
 });
