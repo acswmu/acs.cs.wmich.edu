f5027f8e57a0bab9cd9ccdfb8748010b12c7205b
Cade Gillem <cpgillem@gmail.com>
2016-05-20 20:17:34 -0400

    Rearrange about page to exclude logo.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,19 +6,10 @@
 
 <div class="row">
   <div class="col-sm-12">
-    <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="{{ asset('images/acslogo.png') }}" alt="ACS Logo">
-        </a>
-      </div>
-      <div class="media-body">
-        <h4 class="media-heading">We're the Academic Computing Society at Western Michigan University</h4>
-        <p>Some paragraphs about what we do and stuff</p>
-        <p>Some paragraphs about what we do and stuff</p>
-        ...
-      </div>
-    </div>
+    <h3>We're the Academic Computing Society at Western Michigan University</h3>
+    <p>Some paragraphs about what we do and stuff</p>
+    <p>Some paragraphs about what we do and stuff</p>
+    ...
   </div>
 </div>
 
