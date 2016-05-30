d10b77e7847d59de2b081e02d4467a612d7e6d5b
Cade Gillem <cpgillem@gmail.com>
2016-05-30 16:04:22 -0400

    Add vagrant files.

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -5,7 +5,8 @@
 @section('content')
 
 <div class="row col-sm-12">
-  Schedule Content!
+  <h3>Event Calendar</h3>
+  <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;height=800&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=acswmu%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" style="border-width:0" width="1140" height="800" frameborder="0" scrolling="no"></iframe>
 </div>
 
 @endsection
