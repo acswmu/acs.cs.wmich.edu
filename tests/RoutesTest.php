4c2db31070aaa2a41250481653f0a014765cb887
Cade Gillem <cpgillem@gmail.com>
2016-09-14 17:05:52 -0400

    Add some tests for the routes.

---
+++ tests/RoutesTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RoutesTest extends TestCase
+{
+    public function testGetHome()
+    {
+      $this->visit('/')
+        ->see('<title>Home');
+    }
+
+    public function testGetAbout()
+    {
+      $this->visit('/about')
+        ->see('<title>About Us');
+    }
+
+    public function testGetMembers()
+    {
+      $this->visit('/members')
+        ->see('<title>Members');
+    }
+
+    public function testGetProjects()
+    {
+      $this->visit('/projects')
+        ->see('<title>Projects');
+    }
+
+    public function testGetMeetings()
+    {
+      $this->visit('/meetings')
+        ->see('<title>Meetings');
+    }
+
+    public function testGetSchedule()
+    {
+      $this->visit('/schedule')
+        ->see('<title>Schedule');
+    }
+}
