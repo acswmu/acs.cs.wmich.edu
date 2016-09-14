8e117b92d29731da080a9f62773c828bbfe6faa8
Cade Gillem <cpgillem@gmail.com>
2016-09-14 17:55:34 -0400

    Add migration for more data about users.

---
+++ database/migrations/2016_09_14_212054_add_fields_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('bio')->nullable();
+            $table->string('website')->nullable();
+            $table->boolean('admin')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('bio');
+            $table->dropColumn('website');
+            $table->dropColumn('admin');
+        });
+    }
+}
