78516a0690778a338ee4c4129902ddfec9e08b0f
Cade Gillem <cpgillem@gmail.com>
2016-09-15 18:52:13 -0400

    Clean up minimal members page.

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -18,50 +18,26 @@
     <h3>Executive Board <small>2015-2016</small></h3>
 
     <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="..." alt="...">
-        </a>
-      </div>
       <div class="media-body">
         <h4 class="media-heading">Brandon Kelly <small>President</small></h4>
-        <p>Bio about Brandon Kelly</p>
       </div>
     </div>
 
     <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="..." alt="...">
-        </a>
-      </div>
       <div class="media-body">
         <h4 class="media-heading">Matthew Knapper <small>Vice President</small></h4>
-        <p>Bio about Matthew Knapper</p>
       </div>
     </div>
 
     <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="..." alt="...">
-        </a>
-      </div>
       <div class="media-body">
         <h4 class="media-heading">J.C. Nelson III <small>Treasurer</small></h4>
-        <p>Bio about J.C. Nelson III</p>
       </div>
     </div>
 
     <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="..." alt="...">
-        </a>
-      </div>
       <div class="media-body">
         <h4 class="media-heading">Blake Wrege <small>Public Relations Officer</small></h4>
-        <p>Bio about Blake Wrege</p>
       </div>
     </div>
   </div>
@@ -71,26 +47,14 @@
     <h3>Other Officers <small>2015-2016</small></h3>
 
     <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="..." alt="...">
-        </a>
-      </div>
       <div class="media-body">
         <h4 class="media-heading">Angelo Danducci <small>Secretary</small></h4>
-        <p>Bio about Angelo Danducci</p>
       </div>
     </div>
 
     <div class="media">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" width="128px" src="..." alt="...">
-        </a>
-      </div>
       <div class="media-body">
         <h4 class="media-heading">Cade Gillem <small>Webmaster</small></h4>
-        <p>I appreciate a good meme</p>
       </div>
     </div>
   </div>
