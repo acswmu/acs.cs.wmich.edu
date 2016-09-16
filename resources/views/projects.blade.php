1ea6e4e245b4bdea4406564ffe3f335c568d3832
Cade Gillem <cpgillem@gmail.com>
2016-09-15 22:16:43 -0400

    Add filler content to projects page.

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -5,7 +5,12 @@
 @section('content')
 
 <div class="row">
-
+  <div class="col-sm-12">
+    <h3>Projects</h3>
+    <p>
+      As an ambitious student organization, we are willing to take on various projects. Our current and past projects will be listed here.
+    </p>
+  </div>
 </div>
 
 @endsection
