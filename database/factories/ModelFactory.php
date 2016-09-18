83a2b262a97fd562f5797db41499fb14781d4cc1
Cade Gillem <cpgillem@gmail.com>
2016-09-18 11:57:00 -0400

    Add a model factory for users and a seeder.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,6 +15,8 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
+        'bio' => $faker->paragraph,
+        'website' => $faker->url,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
