9817c1b5b0015106abe214ac979c9fe78b228854
Cade Gillem <cpgillem@gmail.com>
2016-09-18 16:56:35 -0400

    Add migration for agenda topics.

---
+++ database/migrations/2016_09_18_205128_create_agenda_topics_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgendaTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('agenda_topics', function(Blueprint $table) {
+        /* ID's */
+        $table->increments('id');
+        $table->integer('user_id')->unsigned();
+
+        /* Data */
+        $table->string('topic');
+        $table->text('description')->nullable();
+        $table->boolean('old_business')->default(false);
+        $table->boolean('resolved')->default(false);
+
+        /* Dates and times */
+        $table->date('resolved_on')->nullable();
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::drop('agenda_topics');
+    }
+}
