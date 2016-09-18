3faa655f2ab1717d4fb1b5de59d9569c4f3a82fe
Cade Gillem <cpgillem@gmail.com>
2016-09-18 17:03:25 -0400

    Modify migration for agenda topics.

--- database/migrations/2016_09_18_205128_create_agenda_topics_table.php
+++ database/migrations/2016_09_18_205128_create_agenda_topics_table.php
@@ -15,13 +15,14 @@ class CreateAgendaTopicsTable extends Migration
       Schema::create('agenda_topics', function(Blueprint $table) {
         /* ID's */
         $table->increments('id');
-        $table->integer('user_id')->unsigned();
+        $table->integer('user_id')->unsigned()->nullable();
 
         /* Data */
         $table->string('topic');
         $table->text('description')->nullable();
         $table->boolean('old_business')->default(false);
         $table->boolean('resolved')->default(false);
+        $table->boolean('important')->default(false);
 
         /* Dates and times */
         $table->date('resolved_on')->nullable();
