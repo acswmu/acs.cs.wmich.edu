fe6bdcd65c1ed9927e6538137bf49ff6b1ceab67
Cade Gillem <cpgillem@gmail.com>
2016-09-18 17:20:34 -0400

    Add three-column layout on manage page.

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -3,5 +3,32 @@
 @section('title', 'Manage')
 
 @section('content')
-  test
+  <div class="row">
+    <div class="col-lg-4" id="column-blog">
+      <h3>
+        Your Blog<br/>
+        <small>Showcase your talents on the ACS site.</small>
+      </h3>
+    </div>
+    <div class="col-lg-4" id="column-meetings">
+      <h3>
+        Meetings<br/>
+        <small>Manage agenda topics here.</small>
+      </h3>
+    </div>
+
+    {{ Auth::user()->admin }}
+
+    @if (Auth::user()->admin)
+
+    <div class="col-lg-4" id="column-administration">
+      <h3>
+        Administration<br/>
+        <small>Manage membership and more.</small>
+      </h3>
+    </div>
+
+    @endif
+
+  </div>
 @endsection
