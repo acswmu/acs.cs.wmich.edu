1515b9cce787b2867cbc4c269dda50c6bcc9dd3e
ACS User <acsuser@enterprise>
2016-09-25 17:24:12 -0400

    updated officers

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -8,23 +8,23 @@
 
   <!-- LEFT COLUMN: OFFICERS -->
   <div class="col-sm-12 col-lg-6">
-    <h3>Executive Board <small>2015-2016</small></h3>
+    <h3>Executive Board <small>Fall 2016</small></h3>
 
     <div class="media">
       <div class="media-body">
-        <h4 class="media-heading">Brandon Kelly <small>President</small></h4>
+        <h4 class="media-heading">Cade Gillem <small>President</small></h4>
       </div>
     </div>
 
     <div class="media">
       <div class="media-body">
-        <h4 class="media-heading">Matthew Knapper <small>Vice President</small></h4>
+        <h4 class="media-heading">Daniel John Copenhaver <small>Vice President</small></h4>
       </div>
     </div>
 
     <div class="media">
       <div class="media-body">
-        <h4 class="media-heading">J.C. Nelson III <small>Treasurer</small></h4>
+        <h4 class="media-heading">Grant Gannon <small>Treasurer</small></h4>
       </div>
     </div>
 
@@ -37,17 +37,17 @@
 
   <!-- RIGHT COLUMN -->
   <div class="col-sm-12 col-lg-6">
-    <h3>Other Officers <small>2015-2016</small></h3>
+    <h3>Other Officers <small>Fall 2016</small></h3>
 
     <div class="media">
       <div class="media-body">
-        <h4 class="media-heading">Angelo Danducci <small>Secretary</small></h4>
+        <h4 class="media-heading">Matthew Knapper <small>Secretary</small></h4>
       </div>
     </div>
 
     <div class="media">
       <div class="media-body">
-        <h4 class="media-heading">Cade Gillem <small>Webmaster</small></h4>
+        <h4 class="media-heading">Daniel Billingslea <small>Webmaster</small></h4>
       </div>
     </div>
   </div>
